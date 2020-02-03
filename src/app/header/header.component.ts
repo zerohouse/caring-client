5ec5f9d584996bfa7514ebd57ffb13bf814e598a
zerohouse <parksungho86@gmail.com>
2020-02-03 11:03:43 +0900

    커밋

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'cr-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+    selector: 'cr-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
