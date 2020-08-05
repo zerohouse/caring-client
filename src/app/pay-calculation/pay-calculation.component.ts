731d95d704a20defc2b69b95722b1e13fe16a862
zerohouse <parksungho86@gmail.com>
2020-08-05 16:02:13 +0900

    checked added

--- src/app/pay-calculation/pay-calculation.component.ts
+++ src/app/pay-calculation/pay-calculation.component.ts
@@ -1,17 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'cr-pay-calculation.',
-  templateUrl: './pay-calculation.component.html',
-  styleUrls: ['./pay-calculation.component.sass']
+    selector: 'cr-pay-calculation.',
+    templateUrl: './pay-calculation.component.html',
+    styleUrls: ['./pay-calculation.component.sass']
 })
 
 
 export class Pay_calculationComponent implements OnInit {
-  
-  constructor() { }
+    isFamily = true;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
